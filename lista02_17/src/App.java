32d4f34689a484d460060ca805d3e7f74e8a8752
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2023-05-21 21:12:02 -0300

    Primeiro Commit

---
+++ lista02_17/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = -7;
+
+        do {
+            System.out.println(numero);
+            numero -= 7;
+        } while (numero > -1000);
+    }
+}
