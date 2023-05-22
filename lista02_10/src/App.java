7020ed7ef60c03d6e2b92553168ba9869f6e849f
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2023-05-21 21:08:05 -0300

    Primeiro Commit

---
+++ lista02_10/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Impressão de Numeros Pares");
+
+
+        int i = 1;
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+    }
+    }
+}
