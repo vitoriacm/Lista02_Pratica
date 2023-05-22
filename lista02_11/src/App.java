0c48081083af0a5e776e95c9adc89628baf9c2f5
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2023-05-21 21:08:51 -0300

    Primeiro Commit

---
+++ lista02_11/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Impressão do inverso");
+
+        for (int i = 100; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    }
+
