7f75335da4fd16bdacf6f8f77848ea3657a316dc
Vitoria Cezar <91792762+vitoriacm@users.noreply.github.com>
2023-05-21 21:10:47 -0300

    Primeiro Commit

---
+++ lista02_15/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+         int[][] matriz = new int[10][10];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
